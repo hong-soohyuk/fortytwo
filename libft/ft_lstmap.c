4da9420529d52b6ea944d70717319e24b7e1937a
hong-soohyuk <63241308+hong-soohyuk@users.noreply.github.com>
2022-11-17 15:42:04 +0900

    congratulations: perfect libft

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: soohong <soohong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 14:18:33 by soohong           #+#    #+#             */
-/*   Updated: 2022/11/16 17:28:38 by soohong          ###   ########.fr       */
+/*   Updated: 2022/11/17 15:29:46 by soohong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*head;
 	t_list	*new_node;
+	void	*temp;
 
 	if (lst == 0 || f == 0 || del == 0)
 		return (0);
 	head = 0;
 	while (lst)
 	{
-		new_node = ft_lstnew(f(lst->content));
+		temp = f(lst->content);
+		new_node = ft_lstnew(temp);
 		if (new_node == 0)
 		{
+			del(temp);
 			ft_lstclear(&head, del);
 			return (0);
 		}
