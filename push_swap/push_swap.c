d9cfc1a3adfcdac236b7de626dd11fceee23965e
hong-soohyuk <63241308+hong-soohyuk@users.noreply.github.com>
2023-01-13 01:40:34 +0900

    add: is sorted

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,12 +6,27 @@
 /*   By: soohong <soohong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 10:47:16 by soohong           #+#    #+#             */
-/*   Updated: 2023/01/13 00:39:40 by soohong          ###   ########.fr       */
+/*   Updated: 2023/01/13 01:39:53 by soohong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-#include <stdio.h>
+
+int	is_sorted(t_dequeue *a)
+{
+	t_node	*curr;
+
+	if (a->size == 1)
+		return (1);
+	curr = a->head;
+	while (curr)
+	{
+		if (curr->next != NULL && curr->value > curr->next->value)
+			return (0);
+		curr = curr->next;
+	}
+	return (1);
+}
 
 int	main(int argc, char **argv)
 {
@@ -25,6 +40,8 @@ int	main(int argc, char **argv)
 	b = (t_dequeue *)malloc(sizeof(t_dequeue));
 	cmds = (t_cmds *)malloc(sizeof(t_cmds));
 	init_dequeue(a, b, argv, argc);
+	if (is_sorted(a))
+		exit_safe(a, b, EXIT_SUCCESS);
 	if (argc == 4)
 		hardsort(a, cmds);
 	else
