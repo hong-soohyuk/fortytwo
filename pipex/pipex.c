ba3b73a2db1b5de852255a9c685609caa9fce033
hong-soohyuk <63241308+hong-soohyuk@users.noreply.github.com>
2023-02-03 23:54:33 +0900

    🚽add: util codes for pipex

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -5,12 +5,42 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: soohong <soohong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/18 14:29:50 by soohong           #+#    #+#             */
-/*   Updated: 2023/01/18 14:47:31 by soohong          ###   ########.fr       */
+/*   Created: 2023/02/03 22:45:59 by soohong           #+#    #+#             */
+/*   Updated: 2023/02/03 23:52:48 by soohong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(int argc, char **argv)
+#include "pipex.h"
+
+void	child_process(char *infile, char *cmd, int *pipe_fd, char **envp)
+{}
+
+void	parent_process(char *infile, char *cmd, int *pipe_fd, char **envp)
+{}
+
+int	main(int argc, char **argv, char **envp)
 {
-	
+	int		pipe_fd[2];
+	int		status;
+	pid_t	pid;
+	pid_t	wait_pid;
+
+	if (argc != 5)
+		throw_error("argument error", 1);
+	else
+	{
+		if (pipe(pipe_fd) == -1)
+			throw_error("pipe error", 1);
+		pid = fork();
+		if (pid == 0)
+			child_process(argv[1], argv[2], pipe_fd, envp);
+		else if (pid > 0)
+		{
+			wait_pid = waitpid(pid, &status, WNOHANG);
+			parent_process(argv[4], argv[3], pipe_fd, envp);
+		}
+		else
+			throw_error("process fork fail", 1);
+	}
+	return (0);
 }
---
+++ pipex/pipex.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soohong <soohong@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/03 22:45:52 by soohong           #+#    #+#             */
+/*   Updated: 2023/02/03 23:52:01 by soohong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <string.h>
+# include "./libft/libft.h"
+
+void	exec_cmd(char *cmd, char **envp);
+void	throw_error(char *msg, int status);
+
+#endif
