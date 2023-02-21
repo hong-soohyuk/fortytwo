6a762e41d560651a67f134f095507ac5c0248fd7
Soohyuk Hong <soohong@student.42seoul.kr>
2023-02-21 14:48:33 +0900

    temp

--- fract-ol/includes/fractol.h
+++ fract-ol/includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: soohong <soohong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 14:55:04 by soohong           #+#    #+#             */
-/*   Updated: 2023/02/20 15:10:38 by soohong          ###   ########.fr       */
+/*   Updated: 2023/02/21 14:27:25 by soohong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 # define FRACTOL_H
 
 # include <math.h>
-# include "../mlx/mlx.h"
-// # include "libft"
+# include "../libmlx/mlx.h"
+# include "../libft/includes/libft.h"
 
 
 # define TITLE		"fract-ol"
-# define HEIGHT		800
-# define WIDTH		800
+# define HEIGHT		1080
+# define WIDTH		1920
 # define HELP_COLOR	800
 
 enum	e_fractol
@@ -33,32 +33,17 @@ enum	e_fractol
 	FLOWERBROT
 };
 
-typedef struct s_complex
-{
-	double	real;
-	double	image;
-}	t_complex;
-
-typedef struct s_position
-{
-	int	x;
-	int	y;
-}t_position;
-
-typedef struct s_colored_vector
-{
-	double			x;
-	double			y;
-	unsigned int	color;
-}	t_colered_vector;
-
-typedef struct s_image
+typedef struct s_mlx
 {
 	void	*mlx;
 	void	*window;
 	int		*image;
+}	t_mlx;
+
+typedef struct s_image
+{
 	char	*data;
-	int		bpp;
+	int		bits_per_pixel;
 	int		sizeline;
 	int		endian;
 }	t_image;
